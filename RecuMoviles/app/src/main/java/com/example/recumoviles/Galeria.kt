232a721bdd756fbf5ec7842bba5cadce59fd4355
imanol <iperezadam1B@gmail.com>
2024-03-15 00:25:45 +0100

    Proyecto creado

---
+++ RecuMoviles/app/src/main/java/com/example/recumoviles/Galeria.kt
@@ -0,0 +1,13 @@
+package com.example.recumoviles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Galeria : AppCompatActivity() {
+    private lateinit var galeria:String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_galeria)
+    }
+}
